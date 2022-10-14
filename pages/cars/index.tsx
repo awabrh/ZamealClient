d242bb69f98dd5af5270bafc478f60d25f48bd50
Awab Rhamtalla <awabrh@gmail.com>
2022-10-14 10:09:22 +0200

    deleted exmple pages and added constants

--- pages/cars/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function index() {
-  return (
-    <div>cars list</div>
-  )
-}
-
-export default index
--- pages/hello.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function hello() {
-  return (
-    <div>hello world</div>
-  )
-}
-
-export default hello
