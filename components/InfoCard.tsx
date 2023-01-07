936f50c9ac44b7227305fcd3a5504ff648f64109
Awab Rhamtalla <awabrh@gmail.com>
2023-01-07 21:33:15 +0200

    fix wrong row errors

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -1,68 +1,45 @@
-import React from 'react'
+import React from "react";
 
 interface InfoCardProps {
-    type? : 'driver' | 'car' | 'tarheel'
-    values :string[] 
-    className ? : string
+  type?: "driver" | "car" | "tarheel";
+  values: string[];
+  className?: string;
 }
 
-
-function InfoCard({type, values, className} : InfoCardProps) {
-    var labels : JSX.Element
-    var title : string
-    if(type === 'driver') {
-        title = 'معلومات السائق :'
-        labels = 
-        <>
-            <p>اسم السائق</p>
-            <p>السكن</p>
-            <p>رقم الهاتف</p>
-        </>
-    } else if(type === 'car') {
-        title = 'معلومات المركبة :'
-        labels = 
-        <>
-            <p>الموديل</p>
-            <p>عدد المقاعد</p>
-            <p>التكييف</p>
-        </>
-    } else {
-        title = 'معلومات الترحيل :'
-        labels = 
-        <>
-            <p>المواعيد</p>
-            <p>الأيام</p>
-            <p>المناطق</p>
-        </>
-    }
-
-    
-
-    
-
+function InfoCard({ type, values, className }: InfoCardProps) {
+  var labels: String[];
+  var title: string;
+  if (type === "driver") {
+    title = "معلومات السائق :";
+    labels = ["اسم السائق", "السكن", "رقم الهاتف"];
+  } else if (type === "car") {
+    title = "معلومات المركبة :";
+    labels = ["الموديل", "عدد المقاعد", "التكييف"];
+  } else {
+    title = "معلومات الترحيل :";
+    labels = ["المواعيد", "الأيام", "المناطق"];
+  }
 
   return (
     <div className={className}>
-        <div className='relative flex  border-primary gap-3 py-6 px-4 rounded-lg border-2 font-light w-full md:w-96'>
-            <h3 className='absolute -top-4 right-8 bg-black px-3 font-bold text-lg'>
-                {title}
-            </h3>
-            <div id='labels' className='font-bold flex-col '>
-                {labels}   
-            </div>
-            <div id='commas'>
-                <p>:</p>
-                <p>:</p>
-                <p>:</p>
-            </div>
-            <div id='values'>
-                <div>{values[0]}</div>
-                <div>{values[1]}</div>
-                <div>{values[2]}</div>
-            </div>
+      <div className="relative flex border-primary gap-3 py-6 px-4 rounded-lg border-2 font-light w-full md:w-96">
+        <h3 className="absolute -top-4 right-8 bg-black px-3 font-bold text-lg">
+          {title}
+        </h3>
+        <div className="grid grid-cols-12 gap-2">
+          <p className="font-bold col-span-4">{labels[0]}</p>
+          <p className="">:</p>
+          <p className=" col-span-7">{values[0]}</p>
+          <p className="font-bold col-span-4">{labels[1]}</p>
+          <p className="">:</p>
+          <p className=" col-span-7">{values[1]}</p>
+          <p className="font-bold col-span-4">{labels[2]}</p>
+          <p className="">:</p>
+          <p className="col-span-7">{values[2]}</p>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default InfoCard
+export default InfoCard;
