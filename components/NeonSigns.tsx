258b8c816fc42b2a35af542414ae4a90a98d05e3
Awab Rhamtalla <awabrh@gmail.com>
2023-01-14 19:41:11 +0200

    fix neon hierarchy

--- components/NeonSigns.tsx
+++ components/NeonSigns.tsx
@@ -8,32 +8,32 @@ function NeonSigns() {
       <object
         type="image/svg+xml"
         data="/redArrows.svg"
-        className="absolute -top-52 -left-14 -z-30"
+        className="absolute -top-44 -left-28"
       />
       <object
         type="image/svg+xml"
         data="/blueArrow.svg"
-        className="absolute -scale-x-100 -right-[14rem] top-[18rem] -z-30"
+        className="absolute -scale-x-100 -right-[14rem] top-[18rem]"
       />
       <object
         type="image/svg+xml"
         data="/hereSign.svg"
-        className="absolute -top-[18rem] right-20 -z-30"
+        className="absolute -top-[18rem] right-20 "
       />
       <object
         type="image/svg+xml"
         data="/yellowSign.svg"
-        className="absolute top-[20rem] left-8 scale-75 -z-30"
+        className="absolute top-[20rem] left-8 scale-75 "
       />
       <object
         type="image/svg+xml"
         data="/greenArrow.svg"
-        className="absolute -top-[12rem] -right-[9rem] -z-30"
+        className="absolute -top-[12rem] -right-[9rem] "
       />
       <object
         type="image/svg+xml"
         data="/hand.svg"
-        className="absolute top-[13rem] right-[10rem] scale-50 rotate-[-20deg] -z-30"
+        className="absolute top-[13rem] right-[10rem] scale-50 rotate-[-20deg] "
       />
     </div>
   );
